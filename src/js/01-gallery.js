f67ba9f007659bf0ccb0f724fb30704ca88933bb
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-06-16 09:43:09 +0300

    add 1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryUl = document.querySelector(".gallery");
+const markup = createGalleryItems(galleryItems);
+
+galleryUl.style.listStyle = 'none';
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    }).join("");
+}
+
+galleryUl.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: "alt",
+});
