b6a813f51f5a9ac62e51043aaa012088f0a23211
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-06-19 00:24:22 +0300

    add 2 and 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from '/node_modules/lodash.throttle';
 
+const formEl = document.querySelector(".feedback-form");
+const localData = localStorage.getItem("feedback-form-state");
+
+formEl.addEventListener("input", throttle(onInput, 500));
+
+formEl.addEventListener("submit", onSubmit);
+
+if (localData) {
+    const parseData = JSON.parse(localData);
+
+    formEl.elements.email.value = parseData.email;
+    formEl.elements.message.value = parseData.message;   
+}
+
+function onInput(evt) {
+    evt.preventDefault();
+    const { email, message } = formEl.elements;
+    
+    const data = {
+        email: email.value,
+        message: message.value,
+    }
+        
+        localStorage.setItem("feedback-form-state", JSON.stringify(data));      
+}
+
+function onSubmit(evt) {
+    evt.preventDefault();
+
+    console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+
+    localStorage.clear();
+    formEl.reset();
+}
